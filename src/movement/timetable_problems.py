0eda510802882c5a50facc5f9f2dfac35ae0eb70
jjurinci <juricajurincic5@gmail.com>
2022-03-18 11:57:15 +0100

    timetable_problems.py now generates problems properly

--- src/movement/timetable_problems.py
+++ src/movement/timetable_problems.py
@@ -9,17 +9,24 @@ in their room.
 def analyze_timetable():
     state = load_state()
     timetable = state.timetable
+    rasp_rrules = state.rasp_rrules
+    rooms_occupied = state.mutable_constraints.rooms_occupied
+    profs_occupied = state.mutable_constraints.profs_occupied
+    nasts_occupied = state.mutable_constraints.nasts_occupied
 
     for rasp, slot in timetable.items():
-        room_id, week, day, hour = slot
-        send_slot = (week, day, hour)
-        if grade_tool.is_room_problematic(state, rasp, room_id, send_slot):
-            print(f"{rasp.id} has a room collision at {room_id} {week},{day},{hour}")
-        if grade_tool.is_prof_problematic(state, rasp, send_slot):
-            print(f"{rasp.id} has a professor collision at {rasp.professor_id} {week},{day},{hour}")
-        if grade_tool.is_nast_problematic(state, rasp, send_slot):
-            sem_ids = rasp.mandatory_in_semester_ids + rasp.optional_in_semester_ids
-            print(f"{rasp.id} has a semester collision at some of the {sem_ids} {week},{day},{hour}")
+        room_id, _, _, _ = slot
+        for week, day, hour in rasp_rrules[rasp.id]["all_dates"]:
+            for hr in range(hour + rasp.duration):
+                if rooms_occupied[room_id][week, day, hr] > 1:
+                    print(f"{rasp.id} has a room collision at {room_id} {week},{day},{hr}")
+                if profs_occupied[rasp.professor_id][week, day, hr] > 1:
+                    print(f"{rasp.id} has a professor collision at {rasp.professor_id} {week},{day},{hr}")
+                sem_ids = rasp.mandatory_in_semester_ids + rasp.optional_in_semester_ids
+                for sem_id in sem_ids:
+                    if nasts_occupied[sem_id][week, day, hr] > 1:
+                        print(f"{rasp.id} has a semester collision at {sem_id} at {week},{day},{hr}")
+
         if grade_tool.is_capacity_problematic(state, rasp, room_id):
             print(f"{rasp.id} has a capacity problem at {room_id} {week},{day},{hour}")
         if grade_tool.is_computer_problematic(state, rasp, room_id):
